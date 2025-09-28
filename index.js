7d99db00ccf4507c73a3520d2a1c442810776ec6
DaanyaalBawla <daanyaal.b231@gmail.com>
2025-09-28 17:11:20 -0400

    adding zoom function not tested yet

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cameraButton = document.getElementById("cambutton");
 const video = document.getElementById("video");
+const zoom = document.querySelectorAll('.zoom button')
 let streaming = false;
 const constraints = {
     video: {facingMode: { exact: "environment" },}
@@ -9,5 +10,21 @@ cameraButton.addEventListener("click", () =>{
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
       video.srcObject = stream;
       streaming = true
+      track = stream.getVideoTracks()[0];
+      
     })
 })
+function zoomFactor(x) {
+  zoom.forEach(btn => {
+    btn.addEventListener("click", () => {
+      if (btn.id === 'zoom2') {
+        x = 2
+      }
+      if (btn.id === 'zoom4') {
+        x = 4
+      } 
+    })
+    
+  });
+  track.applyConstraints({ advanced: [{ zoom: x}] })
+}
