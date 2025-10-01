8fe441c3e62ae537c3dd7b39c4d207a02a728937
DaanyaalBawla <daanyaal.b231@gmail.com>
2025-10-01 19:41:21 -0400

    updated zoom function and searched for zoom capabilites

--- index.js
+++ index.js
@@ -10,21 +10,25 @@ cameraButton.addEventListener("click", () =>{
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
       video.srcObject = stream;
       streaming = true
-      track = stream.getVideoTracks()[0];
+     const track = stream.getVideoTracks()[0];
+      const capabilities = track.getCapabilities();
+      console.log(capabilities)
+      if ('zoom' in capabilities) {
+        zoomFactor(track)
+      }
       
     })
 })
-function zoomFactor(x) {
+function zoomFactor(track) {
   zoom.forEach(btn => {
     btn.addEventListener("click", () => {
       if (btn.id === 'zoom2') {
-        x = 2
+        track.applyConstraints({ advanced: [{ zoom: 2}] })
       }
       if (btn.id === 'zoom4') {
-        x = 4
+        track.applyConstraints({ advanced: [{ zoom: 4}] })
       } 
     })
     
-  });
-  track.applyConstraints({ advanced: [{ zoom: x}] })
+  })
 }
